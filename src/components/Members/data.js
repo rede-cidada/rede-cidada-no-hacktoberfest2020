d367c44c448b685ea448d1d1a6785b9235ba1814
freitasrayani <freitas.rayani1@gmail.com>
2020-10-09 20:21:33 -0300

    adiçao do nome e participaçao

--- src/components/Members/data.js
+++ src/components/Members/data.js
@@ -31,4 +31,19 @@ export const members = [
       linkedin: 'https://www.linkedin.com/in/licensed/',
     },
   },  
+  {
+    nome: 'Rayani Freitas',
+    participante: 'Aluna',
+    curso: 'Front-end',
+    turno: 'noite',
+    professor: '',
+    foto: {
+      src:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHMrDmh3LkV-g/profile-displayphoto-shrink_400_400/0?e=1606953600&v=beta&t=mzvA93aFahZR0CWM2aq5Lf0ZSfeLKtUrQQMjk_Erk0o',
+      alt: 'Uma mulher palestrando em um grande evento',
+    },
+    redesSocias: {
+      github: 'https://github.com/simoneas02',
+      linkedin: 'https://www.linkedin.com/in/simoneas02/',
+    },
 ];
