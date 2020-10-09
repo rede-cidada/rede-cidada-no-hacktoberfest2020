54da05123d9ed2040d9d183089bc373d5d312e94
mflilian <mayhhara@hotmail.com.br>
2020-10-09 20:54:05 -0300

    Adding myself

--- src/components/Members/data.js
+++ src/components/Members/data.js
@@ -16,10 +16,10 @@ export const members = [
     },
   },
   {
-    nome: 'Ricardo Dantas',
-    participante: 'Professor',
-    curso: 'Python',
-    turno: 'noite',
+    nome: 'Mayhhara Morais',
+    participante: 'Aluna',
+    curso: '',
+    turno: '',
     professor: '',
     foto: {
       src:
@@ -31,36 +31,4 @@ export const members = [
       linkedin: 'https://www.linkedin.com/in/licensed/',
     },
   },  
-  {
-    nome: 'Vanessa Oliveira',
-    participante: 'Aluna',
-    curso: 'Front-end',
-    turno: 'noite',
-    professor: '',
-    foto: {
-      src:
-        'https://scontent.frec26-1.fna.fbcdn.net/v/t1.0-9/35766663_1981589155206921_1106722478434025472_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=TFBYthPHUFIAX_hYCeQ&_nc_ht=scontent.frec26-1.fna&oh=edcd7a03d8a33e55fb86fd007453f7ff&oe=5FA6A3C8',
-      alt: 'licensed',
-    },
-    redesSocias: {
-      github: 'https://github.com/licensed',
-      linkedin: 'https://www.linkedin.com/in/licensed/',
-    },
-  },
-  {
-    nome: 'Ana Beatriz',
-    participante: 'Aluna',
-    curso: 'Front-end',
-    turno: 'Noite',
-    professor: 'Simone',
-    foto: {
-      src:
-        'https://avatars1.githubusercontent.com/u/48129096?s=460&u=9ad8a362NCMhN8ebeDS39jHTMor3xJoiW7mjSK23&v=4',
-      alt: 'Mulher negra de cabelos longos',
-    },
-    redesSocias: {
-      github: '',
-      linkedin: '',
-    },
-  }, 
-];
+  ];
