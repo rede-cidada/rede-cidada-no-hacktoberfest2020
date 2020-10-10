77899e7838866db68bb7dd2f4aa092ca77693c5d
mflilian <mayhhara@hotmail.com.br>
2020-10-09 21:14:42 -0300

    solving

--- src/components/Members/data.js
+++ src/components/Members/data.js
@@ -22,12 +22,12 @@ export const members = [
     turno: '',
     professor: '',
     foto: {
-      src:'https://media-exp1.licdn.com/dms/image/C4E03AQHMrDmh3LkV-g/profile-displayphoto-shrink_400_400/0?e=1606953600&v=beta&t=mzvA93aFahZR0CWM2aq5Lf0ZSfeLKtUrQQMjk_Erk0o',
-      alt: 'Uma mulher palestrando em um grande evento',
+      src:'',
+      alt: '',
     },
     redesSocias: {
-      github: 'https://github.com/simoneas02',
-      linkedin: 'https://www.linkedin.com/in/simoneas02/',
+      github: '',
+      linkedin: '',
     },
   },  
   ];
