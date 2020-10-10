237efb5dbc4377f85d24fa9ef8f6de184eab2b54
freitasrayani <freitas.rayani1@gmail.com>
2020-10-09 21:51:30 -0300

    resolvendo conflitos

--- src/components/Members/data.js
+++ src/components/Members/data.js
@@ -32,39 +32,6 @@ export const members = [
     },
   },
   {
-<<<<<<< HEAD
-=======
-    nome: 'Wendell Matheus',
-    participante: 'Aluno',
-    curso: '',
-    turno: '',
-    professor: '',
-    foto: {
-        src: '',
-        alt: '',
-    },
-    redesSocias: {
-      github: '',
-      linkedin: '',
-    },
-  },
-    {
-    nome: 'Israel Batista',
-    participante: 'Aluno',
-    curso: 'Front-end',
-    turno: 'Noite',
-    professor: 'Simone',
-    foto: {
-      src: '',
-      alt: '',
-    },
-     redesSocias: {
-      github: '',
-      linkedin: '',
-    },
-  },
-  {
->>>>>>> fa0fd1e766af3451468a62cf18b2ca403af1bdd9
     nome: 'Tiago Augusto',
     participante: 'Aluno',
     curso: 'Front-end',
@@ -78,11 +45,7 @@ export const members = [
       github: '',
       linkedin: '',
     },
-<<<<<<< HEAD
   },
-=======
-  },  
->>>>>>> fa0fd1e766af3451468a62cf18b2ca403af1bdd9
   {
     nome: 'Rayani Freitas',
     participante: 'Aluna',
@@ -91,7 +54,6 @@ export const members = [
     professor: 'Simone Amorim',
     foto: {
       src:
-<<<<<<< HEAD
         'https://media-exp1.licdn.com/dms/image/C4E03AQHMrDmh3LkV-g/profile-displayphoto-shrink_400_400/0?e=1606953600&v=beta&t=mzvA93aFahZR0CWM2aq5Lf0ZSfeLKtUrQQMjk_Erk0o',
       alt: 'aluna start',
     },
@@ -99,14 +61,6 @@ export const members = [
       github: 'https://github.com/simoneas02',
       linkedin: 'https://www.linkedin.com/in/simoneas02/',
     },
-=======
-        'https://avatars0.githubusercontent.com/u/50677428?s=460&u=daee1236878fc93e88c208230a2b17ce18836e8c&v=4',
-      alt: 'Uma menina na frente de uma arvore',
-    },
-    redesSocias: {
-      github: 'https://github.com/nessalive',
-      linkedin: 'https://www.linkedin.com/in/vanessa-oliveira-0036a4154/',
->>>>>>> b8a1b7e32e9624245f654eeab17eb93c723c3ac7
     },
   {
     nome: 'Vanessa Oliveira',
@@ -115,12 +69,12 @@ export const members = [
     turno: 'noite',
     professor: '',
     foto: {
-      src: 'https://media-exp1.licdn.com/dms/image/C4E03AQHMrDmh3LkV-g/profile-displayphoto-shrink_400_400/0?e=1606953600&v=beta&t=mzvA93aFahZR0CWM2aq5Lf0ZSfeLKtUrQQMjk_Erk0o',
-      alt: 'Uma mulher palestrando em um grande evento',
+      'https://avatars0.githubusercontent.com/u/50677428?s=460&u=daee1236878fc93e88c208230a2b17ce18836e8c&v=4',
+      alt: 'Uma menina na frente de uma arvore',
     },
     redesSocias: {
-      github: 'https://github.com/simoneas02',
-      linkedin: 'https://www.linkedin.com/in/simoneas02/',
+      github: 'https://github.com/nessalive',
+      linkedin: 'https://www.linkedin.com/in/vanessa-oliveira-0036a4154/',
     },
   },
   {
@@ -167,8 +121,6 @@ export const members = [
     professor: 'Tia Simone <3',
     foto: {
       src:
-<<<<<<< HEAD
-=======
         'https://avatars0.githubusercontent.com/u/58551483?s=400&u=1571a94aa96fcac9d742b207b1ebd7254eaa2123&v=4',
       alt: 'Jovem alto de jaqueta,  em frente a um lago! ',
     },
@@ -185,7 +137,6 @@ export const members = [
     professor: '',
     foto: {
       src:
->>>>>>> 3d215fe2bb9eca17a6b0fa3de112fd617bff0fd5
         '',
       alt: '',
     },
@@ -193,9 +144,5 @@ export const members = [
       github: '',
       linkedin: '',
     },
-<<<<<<< HEAD
-  },   
-=======
   },  
->>>>>>> 3d215fe2bb9eca17a6b0fa3de112fd617bff0fd5
 ];
